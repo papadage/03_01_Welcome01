52b3f0f16723d455e1ee1a9c3493b0657ea40f24
papadage <gpapadakis@aegis-net.com>
2017-05-23 22:48:20 -0400

    Add project files.

---
+++ Welcome01/Welcome01.cs
@@ -0,0 +1,59 @@
+﻿// Assignment 03.01: Welcome 01
+// Purpose: Displays text in a console window
+using System;
+
+// One or more classes needed
+class Welcome01
+{
+   // Main method begins execution of C# app
+   static void Main() 
+   {   // Delimits body of Main method
+
+   }   // Ends the Main method
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+
+//namespace Welcome01
+//{
+//   class Welcome01
+//   {
+//      static void Main()
+//      {
+//         Console.WriteLine("Welcome to C# Programming!");
+//      }
+//   }
+//}
+
+
+
+
+//using System;
+
+
+//namespace Welcome01
+//{
+//   class Welcome01
+//   {
+//      static void Main(string[] args)
+//      {
+//         Console.WriteLine("Welcome to C# Programming!");
+//      }
+//   }
+//}

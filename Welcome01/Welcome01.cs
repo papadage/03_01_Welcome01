7fd883aabc6252f5cd8d0180c3d73387d626a993
papadage <gpapadakis@aegis-net.com>
2017-06-16 19:31:02 -0400

    Practice session retyping of commends to refresh after lack of study.

--- Welcome01/Welcome01.cs
+++ Welcome01/Welcome01.cs
@@ -1,31 +1,31 @@
 ﻿// Assignment 03.01: Welcome 01
 // Purpose: Displays text in a console window
+
 using System;
 
 // One or more classes needed
 class Welcome01
 {
    // Main method begins execution of C# app
-   static void Main() 
+   static void Main()
    {   // Delimits body of Main method
-
+      Console.WriteLine("Welcome to C# Programming!");  //Console class, WriteLine method and it's argument, the string literal in the ""
    }   // Ends the Main method
-  
-}
+
+} // Ends the class Welcome01
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+//using System;
+//class Welcome01
+//{
+//   static void Main()
+//   {
+//      Console.WriteLine("Welsocome to C# Programming!");
+//   }
+//}
 
 
 //using System;
